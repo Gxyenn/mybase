fc864db52b6199b8571be9a968c012cc7365d3f1
Gxyenn <GxyennDEV@gmail.com>
2025-06-11 19:48:07 +0700

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ownerNumber: ["6283877636168"], 
+  ownerName: "Gxყҽɳɳofc", 
+  botName: "BASE WA",
+  whatsapp: "https://wa.me/6283877636168", 
+  global: {
+    foto: "https://cloudkuimages.guru/uploads/files/681a6b5501ee5.png",
+    menuFoto: ""
+  }
+};
